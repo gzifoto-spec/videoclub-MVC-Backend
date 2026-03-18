89f22573d25ba38854c807e7843e31e28dc9d628
gzifoto-spec <gzifoto@gmail.com>
2026-03-18 13:01:29 +0100

    feat: add DBManager for database connection

--- src/main/java/org/example/config/DBManager.java
+++ src/main/java/org/example/config/DBManager.java
@@ -1,4 +1,34 @@
 package org.example.config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBManager {
+    private static final String URL = "jdbc:mysql://localhost:3306/the_omen";
+    private static final String USER = "root";
+    private static final String PASS = "root";
+
+    private static Connection connection;
+
+    public static Connection initConnection() {
+        try {
+            connection=DriverManager.getConnection(URL,USER,PASS);
+            System.out.println("Conectado a BBDD con éxito!");
+        }
+        catch (SQLException exception) {
+            System.out.println("Conexión BBDD fallida" + exception.getMessage());
+        }
+        return connection;
+    }
+
+public static void closeConnection(){
+        try {
+            connection.close();
+            System.out.println("Conexión cerrada!");
+        }catch (SQLException exception){
+            System.out.println("Error al cerrar la BBDD" + exception.getMessage());
+        }
+    }
+
 }
