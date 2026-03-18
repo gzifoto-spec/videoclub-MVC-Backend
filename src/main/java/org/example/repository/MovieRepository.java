9ea44f4bff244914488805ebf638729a2e0d90e5
gzifoto-spec <gzifoto@gmail.com>
2026-03-18 13:25:02 +0100

    feat: implement MovieRepositoryImpl with JDBC

--- src/main/java/org/example/repository/MovieRepository.java
+++ src/main/java/org/example/repository/MovieRepository.java
@@ -1,4 +1,6 @@
 package org.example.repository;
 
 public interface MovieRepository {
+
+
 }
--- src/main/java/org/example/repository/MovieRepositoryImpl.java
+++ src/main/java/org/example/repository/MovieRepositoryImpl.java
@@ -1,4 +1,27 @@
 package org.example.repository;
 
+import org.example.config.DBManager;
+import org.example.model.Movie;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class MovieRepositoryImpl {
+
+    private Connection connection;
+    public void createMovie(Movie movie) throws SQLException {
+        String querySQLCreate = "INSERT INTO terror.json (titulo, anio, rating, poster, sinopsis) VALUES ('"+ movie.getTitulo() + "','"+ movie.getAnio() +"','" + movie.getRating() + "','"+ movie.getPoster() +"','"+ movie.getSinopsis() +"')";
+
+    try {
+        connection = DBManager.initConnection();
+        Statement statement = connection.createStatement();
+        statement.executeUpdate(querySQLCreate);
+        System.out.println("Película creada");
+    } catch (Exception exception){
+        System.out.println(exception.getMessage());
+    } finally {
+        DBManager.closeConnection();
+    }
+}
+
 }
