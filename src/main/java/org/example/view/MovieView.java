80b7568d7fafa37bced14f3b2caf313ade411b28
gzifoto-spec <gzifoto@gmail.com>
2026-03-18 13:39:29 +0100

    feat: add MovieView

--- src/main/java/org/example/view/MovieView.java
+++ src/main/java/org/example/view/MovieView.java
@@ -1,4 +1,47 @@
 package org.example.view;
 
+import org.example.controller.MovieController;
+import org.example.model.Movie;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class MovieView {
+
+    private MovieController movieController;
+
+    public MovieView(MovieController movieController) {
+        this.movieController = movieController;
+    }
+
+    public void CreateMovieView() throws SQLException {
+        Movie movie = generateMovie();
+        movieController.createMovieController(movie);
+    }
+
+    public Movie generateMovie() {
+        Scanner scanner = new Scanner(System.in).useLocale(java.util.Locale.US);
+
+        System.out.println("Escriba el nombre de la película");
+        String titulo = scanner.nextLine();
+
+        System.out.println("Poner imagen del poster de la película");
+        String poster = scanner.nextLine();
+
+        System.out.println("Escriba la descripción de la película");
+        String sinopsis = scanner.nextLine();
+
+        System.out.println("Escriba el año de la película");
+        Integer anio = scanner.nextInt();
+
+        System.out.println("Escriba el rating de la película");
+        double rating = scanner.nextDouble();
+
+        Movie movie = new Movie(titulo, anio, rating, poster, sinopsis);
+
+        scanner.close();
+
+        return movie;
+    }
+
 }
