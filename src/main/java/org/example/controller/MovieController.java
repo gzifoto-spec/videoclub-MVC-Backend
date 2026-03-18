c75afe82ffa5c7ebaf958c48dfadc3114b4b1fae
gzifoto-spec <gzifoto@gmail.com>
2026-03-18 13:33:36 +0100

    feat: add MovieController

--- src/main/java/org/example/controller/MovieController.java
+++ src/main/java/org/example/controller/MovieController.java
@@ -1,4 +1,20 @@
 package org.example.controller;
 
+import org.example.model.Movie;
+import org.example.repository.MovieRepository;
+import org.example.repository.MovieRepositoryImpl;
+
+import java.sql.SQLException;
+
 public class MovieController {
+
+    private MovieRepositoryImpl movieRepository;
+    public MovieController(MovieRepositoryImpl movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    public void createMovieController(Movie movie) throws SQLException {
+        movieRepository.createMovie(movie);
+    }
+
 }
